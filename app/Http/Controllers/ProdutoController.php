1a7bfd1e0344330a409dbcfdbf3398d4a0649df9
Ricardo Barbosa Baldrez <ricardo.baldrez@gmail.com>
2019-01-05 00:30:37 -0200

    Criação do método mostra() no arquivo ProdutoController, criação de mais uma coluna no arquivo listagem e inclusão CDN do bootstrap e criação da view detalhes

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -4,6 +4,7 @@ namespace estoque\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use estoque\Produto;
+use Request;
 
 class ProdutoController extends Controller
 {
@@ -12,5 +13,18 @@ class ProdutoController extends Controller
         $produtos = DB::select('select * from jogos');
 
         return view('listagem')->with('produtos', $produtos);
+    }
+
+    public function mostra()
+    {
+        $id = Request::route('id');
+
+        $produto = DB::select('select * from jogos where id = ?', [$id]);
+
+        if(empty($produto)) {
+            return '<h3>O produto não existe!</h3>';
+        }
+       
+        return view('detalhes')->with('produto', $produto[0]);
     }
 }
--- resources/views/listagem.php
+++ resources/views/listagem.php
@@ -2,6 +2,8 @@
     <head>
         <link rel="stylesheet" href="/css/app.css">
         <title>Controle do estoque de jogos</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
 
     <body>
@@ -14,8 +16,13 @@
                         <td><?= $produto->descricao ?></td>
                         <td id="middle" class="oneRow"><?= $produto->compatibilidade ?></td>
                         <td id="middle" class="oneRow"><?= $produto->genero ?></td>
-                        <td id="middle"><?= $produto->preco ?></td>
+                        <td id="middle"><?= number_format($produto->preco, 2, ',', '.') ?></td>
                         <td id="middle"><?= $produto->quantidade ?></td>
+                        <td id="middle">
+                            <a href="/produtos/mostra/<?= $produto->id ?>">
+                                <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                            </a>
+                        </td>
                     </tr>
                 <?php endforeach ?>
             </table>
